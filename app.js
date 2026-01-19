b4b413c380435ce86d39f64965abd40ed8a3701b
CipherZemo <acharyakrishnan9@gmail.com>
2026-01-19 13:54:46 +0530

    changed script

--- app.js
+++ app.js
@@ -4,11 +4,16 @@ const express = require('express');
 
 connectDB();
 const app = express();
+app.use(express.json()); // It parses incoming requests with JSON payloads. Without this, you won't be able to access `req.body`
 
 app.get('/', (req, res) => {
   res.send('API is running...');
 });
 
+// --- ADD ROUTE HANDLING ---
+
+const urlRoutes = require('./routes/urls');
+app.use('/api', urlRoutes);// Mount the router: Tell the app to use the 'urlRoutes' for any request
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server is alive and running on http://localhost:${PORT}`));
