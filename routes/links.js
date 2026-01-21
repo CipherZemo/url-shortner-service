c3c564fa69dc24d3a2bc835cd8a3d2c4f555ae9e
CipherZemo <acharyakrishnan9@gmail.com>
2026-01-21 22:06:33 +0530

    route for legit user link and auth

---
+++ routes/links.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const router = express.Router();
+const auth = require('../middleware/auth');
+const { getMyLinks } = require('../controllers/linksController');
+
+router.get('/my-links', auth, getMyLinks);
+
+
+module.exports = router;
