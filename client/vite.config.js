fdab3f53dc8b4c5bd5e0c74a93a8b1d46dbb5b9d
CipherZemo <acharyakrishnan9@gmail.com>
2026-01-25 14:04:52 +0530

    Configured Vite Proxy for backend services

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+// web browsers enforce a strict security rule called the Same-Origin Policy.Since your frontend and backend are on different ports, they are considered different origins, and the browser's security guard will block the communication. Using Vite's built-in proxy, Vite-server will try to make the calls to the backend on your behalf. From the backend's perspective, the request is coming from the Vite(which is part of same project), not from a different origin, so it happily responds. Vite then passes the response back to your code.
 
 export default defineConfig({
   plugins: [react()],
