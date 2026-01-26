98506ae4640fc70284b2f7e240da9da940b10fee
CipherZemo <acharyakrishnan9@gmail.com>
2026-01-26 16:29:51 +0530

    added guest url feature

--- controllers/urlController.js
+++ controllers/urlController.js
@@ -3,7 +3,6 @@ const validUrl = require('valid-url'); //library to validate the format of the i
 
 const shortenUrl = async (req, res) => {
     const { longUrl } = req.body;
-    console.log('Received long URL:', longUrl);
 
     if (!longUrl) {
         return res.status(400).json({ success: false, error: 'Please provide a URL' });
@@ -13,12 +12,12 @@ const shortenUrl = async (req, res) => {
     } //url validation
 
     try {
-        let url = await Url.findOne({ longUrl: longlink }); //1st one is the schema field,2nd one is here's variable 
+        let url = await Url.findOne({ longUrl: longUrl }); //1st one is the schema field,2nd one is here's variable 
         if (url) {
             return res.status(200).json({ success: true, data: url });
         }
         // Dynamically import the 'nanoid' package. This is the modern way to import an ES Module into a CommonJS file within an async function since nanoid are published as ES Modules (ESM), which use the 'import' syntax. You cannot directly require() an ESM package in a CommonJS file
-        const { nanoid } = await import('nanoid');
+        const { nanoid } = await import('nanoid');//A tiny, secure, URL-friendly, unique string ID generator for JavaScript
         const urlCode = nanoid(7);
 
         const shortUrl = `${process.env.BASE_URL}/${urlCode}`;
@@ -52,6 +51,7 @@ const redirectToUrl = async (req, res) => {
         } else {
             return res.status(404).json({ success: false, error: 'No URL found' });
         }
+        
     } catch (err) {
         console.error('Server error on redirect:', err);
         res.status(500).json({ success: false, error: 'Internal Server Error' });
