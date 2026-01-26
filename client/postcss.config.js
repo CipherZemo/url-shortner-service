25bd50df35329ecae937e225a0d8929bd5b921ec
CipherZemo <acharyakrishnan9@gmail.com>
2026-01-26 23:00:03 +0530

    tailwind config file

---
+++ client/postcss.config.js
@@ -0,0 +1,16 @@
+export default {
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+  },
+}
+
+
+
+
+
+
+// This project has tailwind v3 since the npm install command was "npm install -D tailwindcss postcss autoprefixer" and this project was old so the latest patch at that time was v3. The config files were created using "npx tailwindcss init -p". i had to use v3 command to generate the config files(bcuz v4 doesnt support that npx, they changed that way).
+
+// npm install -D tailwindcss@3 postcss autoprefixer
+//npx tailwindcss init -p
